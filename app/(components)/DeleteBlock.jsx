2c2b4d79e5be1c1d18d699cfe7fcd877fce820bb
Suwagath S. <suwagath.srithar@gmail.com>
2025-04-12 21:11:22 +0530

    Add delete functionality to DeleteBlock component with API integration

--- app/(components)/DeleteBlock.jsx
+++ app/(components)/DeleteBlock.jsx
@@ -1,12 +1,29 @@
+"use client";
+
 import { faX } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRouter } from "next/navigation";
 import React from "react";
 
-const DeleteBlock = () => {
+const DeleteBlock = ({ id }) => {
+  const router = useRouter();
+  const deleteTicket = async () => {
+    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (res.ok) {
+      router.refresh();
+    }
+  };
   return (
     <FontAwesomeIcon
       icon={faX}
       className="text-red-400 over:cursor-pointer hover:text-red-200"
+      onClick={deleteTicket}
     />
   );
 };
--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -1,7 +1,23 @@
 import TicketForm from "@/app/(components)/TicketForm";
 import React from "react";
 
-const TicketPage = ({ params }) => {
+const getTicketById = async (id) => {
+  const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch ticket");
+  }
+};
+
+const TicketPage = async ({ params }) => {
+  const EDITMODE = params.id == "new" ? false : true;
+  let updateTicketData = {};
+  if (EDITMODE) {
+    updateTicketData = await getTicketById(params.id);
+    console.log("updateTicketData", updateTicketData);
+  }
   return <TicketForm />;
 };
 
