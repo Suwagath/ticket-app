1416c743e804649e0b9389f6d53773fcbb8563b1
Suwagath S. <suwagath.srithar@gmail.com>
2025-04-12 20:48:46 +0530

    Refactor ticket components to accept props for priority, progress, and status; enhance dashboard functionality with dynamic data rendering

--- app/(components)/StatusDisplay.jsx
+++ app/(components)/StatusDisplay.jsx
@@ -1,9 +1,31 @@
 import React from "react";
 
-const StatusDisplay = () => {
+const StatusDisplay = ({ status }) => {
+  const getColor = (status) => {
+    let color = "bg-slate-700";
+    switch (status.toLowerCase()) {
+      case "not started":
+        color = "bg-red-400";
+        break;
+      case "in progress":
+        color = "bg-yellow-400";
+        break;
+      case "completed":
+        color = "bg-green-400";
+        break;
+      default:
+        color = "bg-slate-700";
+    }
+    return color;
+  };
+
   return (
-    <span className="inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 bg-green-200">
-      Done
+    <span
+      className={`inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 ${getColor(
+        status
+      )}`}
+    >
+      {status}
     </span>
   );
 };
--- app/page.jsx
+++ app/page.jsx
@@ -1,22 +1,34 @@
-import React, { cache } from "react";
+import React from "react";
 import TicketCard from "./(components)/TicketCard";
 
-const getTicket = async () => {
+const getTickets = async () => {
   try {
-    const res = await fetch("https://localshost:3000/api/Tickets", {
+    const res = await fetch("http://localhost:3000/api/Tickets", {
       cache: "no-store",
     });
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch topics");
+    }
+
     return res.json();
   } catch (error) {
-    console.log("Failed to get tickets", error);
+    console.log("Error loading topics: ", error);
   }
 };
 
 const Dashboard = async () => {
-  const { tickets } = await getTicket();
+  const data = await getTickets();
+
+  // Make sure we have tickets needed for production build.
+  if (!data?.tickets) {
+    return <p>No tickets.</p>;
+  }
+
+  const tickets = data.tickets;
 
   const uniqueCategories = [
-    ...new Set(tickets.map(({ category }) => category)),
+    ...new Set(tickets?.map(({ category }) => category)),
   ];
 
   return (
@@ -26,17 +38,17 @@ const Dashboard = async () => {
           uniqueCategories?.map((uniqueCategory, categoryIndex) => (
             <div key={categoryIndex} className="mb-4">
               <h2>{uniqueCategory}</h2>
-              <div className="lg:grid grid-cols-2 xl:grid-cols-4"></div>
-
-              {tickets
-                .filter((ticket) => ticket.category === uniqueCategory)
-                .map((filteredTicket, _index) => (
-                  <TicketCard
-                    id={_index}
-                    key={_index}
-                    ticket={filteredTicket}
-                  />
-                ))}
+              <div className="lg:grid grid-cols-2 xl:grid-cols-4 ">
+                {tickets
+                  .filter((ticket) => ticket.category === uniqueCategory)
+                  .map((filteredTicket, _index) => (
+                    <TicketCard
+                      id={_index}
+                      key={_index}
+                      ticket={filteredTicket}
+                    />
+                  ))}
+              </div>
             </div>
           ))}
       </div>
